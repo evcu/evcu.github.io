4ed020f829fb825bb15327d74b98ca891cd6cc7a
evcu <evcu@users.noreply.github.com>
2017-12-24 22:13:58 -0500

    Todays post to nyc365blog

--- assets/nyc365blog/newDay.py
+++ assets/nyc365blog/newDay.py
@@ -5,12 +5,12 @@ path = '/Users/evcu/GitHub/evcu.github.io//assets/nyc365blog/data.json'
 data = {}
 newel = {}
 newel[u'date'] = str(date.today())
-newel[u'mood'] = unicode(raw_input("Enter mood -1/0/1:\n"))
+newel[u'mood'] = str(raw_input("Enter mood -1/0/1:\n"))
 print(newel)
-newel[u'high'] = unicode(raw_input("Highlights\n"))
-newel[u'low'] = unicode(raw_input("Lowlights:\n"))
-newel[u'other'] = unicode(raw_input("Other:\n"))
-newel[u'text'] = unicode(raw_input("Random Thoughts:\n"))
+newel[u'high'] = str(raw_input("Highlights\n"))
+newel[u'low'] = str(raw_input("Lowlights:\n"))
+newel[u'other'] = str(raw_input("Other:\n"))
+newel[u'text'] = str(raw_input("Random Thoughts:\n"))
 
 print(newel)
 with open(path,'r') as data_file:
